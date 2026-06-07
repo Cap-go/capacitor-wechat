3acb618a092086c3fc0e34f2ca853955e47c02a2
Martin Donadieu <martindonadieu@gmail.com>
2026-06-07 13:13:50 +0200

    style: format capgo example rollout

--- example-app/capacitor.config.ts
+++ example-app/capacitor.config.ts
@@ -3,26 +3,26 @@ import type { CapacitorConfig } from '@capacitor/cli';
 import pkg from './package.json';
 
 const config: CapacitorConfig = {
-  "appId": "app.capgo.wechat",
-  "appName": "WeChat Example",
-  "webDir": "dist",
-  "plugins": {
-    "CapacitorWechat": {
-      "appId": "wx_YOUR_APP_ID",
-      "universalLink": "https://your-universal-link.example.com/"
+  appId: 'app.capgo.wechat',
+  appName: 'WeChat Example',
+  webDir: 'dist',
+  plugins: {
+    CapacitorWechat: {
+      appId: 'wx_YOUR_APP_ID',
+      universalLink: 'https://your-universal-link.example.com/',
     },
-    "SplashScreen": {
-      "launchAutoHide": false
+    SplashScreen: {
+      launchAutoHide: false,
     },
-    "CapacitorUpdater": {
-      "appId": "app.capgo.wechat",
-      "autoUpdate": true,
-      "autoSplashscreen": true,
-      "directUpdate": "always",
-      "defaultChannel": "production",
-      "version": pkg.version
-    }
-  }
+    CapacitorUpdater: {
+      appId: 'app.capgo.wechat',
+      autoUpdate: true,
+      autoSplashscreen: true,
+      directUpdate: 'always',
+      defaultChannel: 'production',
+      version: pkg.version,
+    },
+  },
 };
 
 export default config;
